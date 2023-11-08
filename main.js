4254c9c5603162234416831c7e2dc51336161b2d
WasabiAddict <davidtltran@gmail.com>
2023-11-08 15:30:59 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const calories = document.querySelector(".bmr-calculator .result .calories");
+const calculateBtn = document.querySelector(".bmr-calculator .result .calculate-btn");
+const age = document.querySelector(".bmr-calculator form #age");
+const height = document.querySelector(".bmr-calculator form #height");
+const weight = document.querySelector(".bmr-calculator form #weight");
+const errorMessage = document.querySelector(".bmr-calculator .result .error-message");
+
+// BMR male = 10*weight + 6.25*height - 5*age +5
+// BMR female = 10*weight + 6.25*height -5*age -161
+
+const calculateBMR = function(weight, height, age, gender){
+    if(gender == 'male') {
+        return 10 * weight + 6.25 * height - 5 * age + 5;
+    }
+    return 10 * weight + 6.25 * height -5 * age - 161;
+};
+
+calculateBtn.addEventListener("click", function() {
+
+if(age.classList.contains("invalid") || height.classList.contains("invalid") || weight.classList.contains("invalid")) {
+    errorMessage.classList.add("active");
+    return;
+}
+    errorMessage.classList.remove("active");
+
+    let genderValue = document.querySelector(".bmr-calculator form input[name='gender']:checked").value;
+
+    let BMR =  calculateBMR(weight.value, height.value, age.value, genderValue)
+
+    calories.innerHTML = BMR.toLocaleString("en-US");
+});
+
+//Input Validation
+
+age.addEventListener("input", function(e) {
+    let ageValue = e.target.value;
+
+    if(!ageValue || isNaN(ageValue) || ageValue < 10 || ageValue > 100) {
+        age.classList.add("invalid");
+    } else {
+        age.classList.remove("invalid");
+    };
+});
+
+height.addEventListener("input", function(e) {
+    let heightValue = e.target.value;
+
+    if(!heightValue || isNaN(heightValue) || heightValue < 0) {
+        height.classList.add("invalid");
+    } else {
+        height.classList.remove("invalid");
+    };
+});
+
+weight.addEventListener("input", function(e) {
+    let weightValue = e.target.value;
+
+    if(!weightValue || isNaN(weightValue) || weightValue < 0) {
+        weight.classList.add("invalid");
+    } else {
+        weight.classList.remove("invalid");
+    };
+});

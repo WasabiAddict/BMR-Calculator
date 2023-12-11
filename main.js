675d687302a4d0a624c03380492b70fc261772bb
WasabiAddict <davidtltran@gmail.com>
2023-12-11 17:06:10 -0600

    adding measurement metric imperial to calculateBMR function

--- main.js
+++ main.js
@@ -5,17 +5,31 @@ const weight = document.getElementById("weight");
 const calories = document.getElementById("calories");
 const form = document.getElementById("form");
 const error = document.getElementById("error");
-const units = document.getElementsByName("measurement")
+const units = document.querySelectorAll("input[name='measurement']");
+const cm = document.getElementById('cm');
+const kg = document.getElementById('kg');
 
-console.log(units);
 
+/* Imperial Units
+For men: 66.47 + (6.24 × weight in pounds) + (12.7 × height in inches) − (6.75 × age)
+For women: 65.51 + (4.35 × weight in pounds) + (4.7 × height in inches) - (4.7 × age)
+*/
+
+//Metric Units
 //BMR = 10 * weight + 6.25 * height - 5 * age + 5; male
 //BMR = 10 * weight + 6.25 * height - 5 * age -161; female
-const calculateBMR = function(weight, height, age, gender){
-    if(gender == "male") {
+const calculateBMR = function(weight, height, age, gender, measurement){
+    if(gender == "male" && measurement =="metric") {
        return 10 * weight + 6.25 * height - 5 * age + 5;
-    } 
-       return 10 * weight + 6.25 * height - 5 * age - 161;
+    } else if (gender == "female" && measurement == "metric") {          
+        return 10 * weight + 6.25 * height - 5 * age - 161;
+    } else if (gender == "male" && measurement == "imperial") {
+        return 66.47 + 6.24 * weight + 12.7 * height - 6.75 * age;
+    } else if (gender == "female" && measurement == "imperial") {
+        return 65.51 + 4.35 * weight  + 4.7 * height - 4.7 * age
+    }
+    
+       
 };
 
 
@@ -53,8 +67,16 @@ function validation(genderValue) {
 
 
 //if imperial or metric is selected, show units respectively
-
-function filter() {
-    
+console.log(units)
+for(const unit of units) {
+    unit.onchange = () => {
+        if (units[0].checked == true ) {
+            kg.innerText = 'lbs';
+            cm.innerText = 'in'
+        }else{
+            kg.innerText = 'kg';
+            cm.innerText = 'cm';
+        }
+    }
 }
 
